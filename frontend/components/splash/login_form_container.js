847d4e94ace4aa73f29457960eaeccc8e91ddd62
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 18:02:54 -0400

    Add file structure for splash component

---
+++ frontend/components/splash/login_form_container.js
@@ -0,0 +1,2 @@
+import connect from 'react-redux';
+import { login } from '../../actions/session_api_actions';
---
+++ frontend/components/splash/signup_form_container.js
@@ -0,0 +1,3 @@
+import { connect } from 'react-redux';
+import { signup } from '../../actions/session_api_actions';
+
