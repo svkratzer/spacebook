16259139aedac789c111e5e32c52cafe0b85a0b4
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 14:20:40 -0400

    Build create and show actions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :ensure_logged_in, only: [:show]
+
+  def create
+    @user = User.create(user_params)
+    if @user.save!
+      login!(user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :birthday,
+      :gender,
+      :bio
+    )
+  end
 end
