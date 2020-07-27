e30c956662e8d00159d2766fe0bf54084ee8ecbf
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 13:08:15 -0400

    Build out necessary user auth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,51 @@
 #  bio             :text
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
-#
 
 class User < ApplicationRecord
+  validates :first_name, :last_name, :password_digest, :gender, :birthday, presence: true
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initailize: :ensure_session_token
+
+  # Required for :password validation
+  attr_reader :password
+
+  # Finds user by email and returns user if password matches
+  def self.find_by_credentials(email, password)
+    user = user.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  # Resets session_token, persists change, and returns session_token
+  def reset_session_token!
+    self.session_token = self.class.generate_sesssion_token
+    self.save!
+    self.session_token
+  end
+
+  # Compares plaintext password to password_digest
+  def is_password?(password)
+    bcrypt_password = BCrypt::Password.new(password_digest)
+    bcrypt_password.is_password?(password)
+  end
+
+  # Sets password_digest to 'BCryptified' version of the plaintext password
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private
+  # Generates a random string for session_token
+  def self.generate_sesssion_token
+    SecureRandom::urlsafe_base64
+  end
   
+  # Called with after_initialize callback
+  def ensure_session_token
+    self.session_token ||= self.class.generate_sesssion_token
+  end
+
 end
