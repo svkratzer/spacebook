a7e34c7b1d4489d8044da1bed4f7937fbfc11ee8
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-26 23:02:26 -0400

    Create configureStore function to create and thread store through Root component in myface.jsx

---
+++ frontend/store/store.js
@@ -0,0 +1,12 @@
+const { createStore, applyMiddleware } = require("redux")
+
+import { createStore, applyMiddleware } from 'react-redux';
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import rootReducer from '../reducers/root_reducer'
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+}
+
+export default configureStore;
