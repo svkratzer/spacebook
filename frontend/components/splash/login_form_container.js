1f54867750c322e16c978d5e27203bcc71ac286f
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 18:14:48 -0400

    Create skeleton components

--- frontend/components/splash/login_form_container.js
+++ frontend/components/splash/login_form_container.js
@@ -1,2 +1,20 @@
 import connect from 'react-redux';
 import { login } from '../../actions/session_api_actions';
+import LoginForm from './login_form';
+
+const mSTP = state => {
+  return {
+
+  };
+} 
+
+const mDTP = dispatch => {
+  return {
+
+  };
+}
+
+const LoginFormContainer = connect(mSTP, mDTP)(LoginForm);
+
+export default LoginFormContainer;
+
--- frontend/components/splash/signup_form_container.js
+++ frontend/components/splash/signup_form_container.js
@@ -1,3 +1,19 @@
 import { connect } from 'react-redux';
 import { signup } from '../../actions/session_api_actions';
+import SignupForm from './signup_form';
 
+const mSTP = state => {
+  return {
+
+  };
+}
+
+const mDTP = dispatch => {
+  return {
+
+  };
+}
+
+const SignupFormContainer = connect(mSTP, mDTP)(SignupForm);
+
+export default SignupFormContainer;
