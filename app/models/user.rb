a5f7fddb038023198ffc0d896eaff1263ae85c5d
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 12:42:27 -0400

    Annotate User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  gender          :string           not null
+#  birthday        :date             not null
+#  bio             :text
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  
 end
