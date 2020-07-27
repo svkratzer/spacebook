b81ad7ef6e970ecf77d17213ba1c53c260aa6d25
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-26 23:12:42 -0400

    Import configureStore in entry file and pass store to Root component

--- frontend/myface.jsx
+++ frontend/myface.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from '/store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  // FOR TESTING PURPOSES ONLY
+  window.dispatch = store.dispatch;
+  window.getState = store.getState;
+  // DELETE FOR PRODUCTION
+
   const root = document.getElementById('root');
-  ReactDOM.render(<Root />, root)
+  ReactDOM.render(<Root store={store}/>, root)
 });
