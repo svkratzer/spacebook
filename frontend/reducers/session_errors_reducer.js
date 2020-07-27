54bac29fbe693fac73e4480121e629779e90dadb
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 16:39:20 -0400

    Fix errors state slice bug

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -3,9 +3,7 @@ import {
   RECEIVE_CURRENT_USER,
 } from '../actions/session_api_actions';
 
-const _defaultErrors = [];
-
-const sessionErrorsReducer = (state = _defaultErrors, action) => {
+const sessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
