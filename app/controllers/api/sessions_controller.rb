aab546bdbcdc462f1f88c69696227cb93902b096
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 14:03:09 -0400

    Build create and destroy actions

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
+  before_action :ensure_logged_in, only: [:create]
 
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if @user
+      login!(@user)
+      redirect_to api_user_url(@user)
+    else
+      render json: ['Oops! Invalid email or password. Please, try again.'], status: 401
+  end
+
+  def destroy
+    if current_user
+      logout!
+    else
+      render json: ['You\'re already logged out, ya \'lil peanut!']
+  end
 end
