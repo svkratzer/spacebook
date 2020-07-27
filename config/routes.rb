1b275f89066ae5a8f2b8e267bdd01c1b4bbcc383
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-26 23:40:45 -0400

    Fix StaticPagesController render problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Leave root route at the top, because it should be matched first.
   root to: 'static_pages#root'
-
 end
-
-# npm init -y
-# npm install webpack webpack-cli react react-dom react-redux redux redux-logger @babel/core @babel/preset-react @babel/preset-env babel-loader redux-logger react-router-dom
