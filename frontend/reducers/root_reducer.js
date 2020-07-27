601ea5f4738390b989bd80813fccc9da8488f61c
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 16:04:52 -0400

    Connect reducers in root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import sessionReducer from './session_reducer'
+import errorsReducer from './session_errors_reducer';
+import entitiesReducer from './entities_reducer';
 
 const rootReducer = combineReducers({
-  session: sessionReducer
+  entities: entitiesReducer,
+  session: sessionReducer,
+  errors: errorsReducer
 });
 
 export default rootReducer;
