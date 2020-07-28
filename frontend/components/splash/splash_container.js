18500310fde27d0309dbb0176485f7703faa4c5f
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-28 08:35:58 -0400

    Create SplashContainer to give Splash access to login thunk action creator

---
+++ frontend/components/splash/splash_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { login } from '../../actions/session_api_actions';
+import Splash from './splash'
+
+const mDTP = dispatch => {
+  return {
+    login: user => dispatch(login(user))
+  };
+};
+
+const SplashContainer = connect(null, mDTP)(Splash);
+
+export default SplashContainer;
