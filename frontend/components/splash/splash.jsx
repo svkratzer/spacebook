0ec2403a9213caee9f78052f3c403a1d4f545caa
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-28 09:14:14 -0400

    Fix import bug

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,6 +3,20 @@ import LoginFormContainer from './login_form_container';
 import SignupFormContainer from './signup_form_container';
 
 class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.demoUserCredentials = {
+      email: "thecage@nick.com",
+      password: "password123"
+    }
+
+    this.loginDemoUser = this.loginDemoUser.bind(this);
+  }
+
+  loginDemoUser() {
+    this.props.login(this.demoUserCredentials);
+  }
   
   render() {
     return (
@@ -15,7 +29,9 @@ class Splash extends React.Component {
         <div>
           <h2>Sign Up</h2>
           <SignupFormContainer />
+          <button onClick={() => this.loginDemoUser}>Nicholas Cage Simulator</button>
         </div>
+
       </div>
     );
   }
