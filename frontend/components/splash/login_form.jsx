47f06c51e9a627df12d8b4d6e3b50e21d32bc3b5
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-27 22:14:42 -0400

    Create first rendition of login form

--- frontend/components/splash/login_form.jsx
+++ frontend/components/splash/login_form.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
+import { update } from 'lodash';
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e =>
+      this.setState({ [field]: this.currentTarget.value })
+  }
+
+  handleSubmit() {
+    this.props.login(this.state);
   }
 
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}
+        className="login-form">
+
+        <div className="login-email-container">
+          <div className="login-email-label">Email</div>
+
+          <input type="text"
+            value={this.state.email}
+            className="login-email-input"
+            onChange={this.update('email')} />
+        </div>
+
+        <div className="login-passowrd-container">
+          <div className="login-password-label">Password</div>
+          
+          <input type="password"
+            value={this.state.password}
+            className="login-password-input"
+            onChange={this.update('password')}/>
+        </div>
         
-      </div>
+        <button>Log In</button>
+      </form>
     );
   }
 }
