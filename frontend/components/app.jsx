51377fee02017d0d637ae0b58d91e8f937223b08
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-28 11:28:37 -0400

    Create placeholder 404 page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import SplashContainer from './splash/splash_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Route, Switch } from 'react-router-dom';
 import ProfileShowContainer from './profile/profile_show_container';
+import FourZeroFour from './error_page/four_zero_four';
 
 const App = () => (
   <div>
-    <AuthRoute exact path="/" component={SplashContainer} />
-    <ProtectedRoute path="/newsfeed" component={ProfileShowContainer} />
+    <Switch>
+      <AuthRoute exact path="/" component={SplashContainer} />
+      <ProtectedRoute path="/newsfeed" component={ProfileShowContainer} />
+      <Route component={FourZeroFour}/>
+    </Switch>
   </div>
 );
 
---
+++ frontend/components/error_page/four_zero_four.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+class FourZeroFour extends React.Component {
+  
+  render() {
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    return(
+      null
+    );
+  }
+}
+
+export default FourZeroFour;
