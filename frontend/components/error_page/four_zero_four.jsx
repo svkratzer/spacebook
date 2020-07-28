3e9f6dbd12da845125e842d72ad278a3bc43805c
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-28 11:50:30 -0400

    ready for heroku push

--- frontend/components/error_page/four_zero_four.jsx
+++ frontend/components/error_page/four_zero_four.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 class FourZeroFour extends React.Component {
   
   render() {
-    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     return(
-      null
+      <div>error 404</div>
     );
   }
 }
