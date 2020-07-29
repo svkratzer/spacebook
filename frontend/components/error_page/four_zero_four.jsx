7eb212db1d89264a276b012572fe34fbd6026675
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-28 23:19:39 -0400

    Build 404 page

--- frontend/components/error_page/four_zero_four.jsx
+++ frontend/components/error_page/four_zero_four.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 
 class FourZeroFour extends React.Component {
   
+  componentDidMount() {
+    setInterval(() => window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 5000);
+  }
+
   render() {
-    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     return(
-      <div>erro 404</div>
+      <div className="error-page">
+        <marquee className="blinking" behavior="" direction="">DANGER DANGER DANGER</marquee>
+        <div>
+          <h1>404</h1>
+          <h2>Ya done goofed ¯\_(ツ)_/¯</h2>
+        </div>
+        
+        <marquee className="blinking" behavior="" direction="">DANGER DANGER DANGER</marquee>
+      </div>
     );
   }
 }
