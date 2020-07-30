54fe533d2ff46cd604717115fe2276af0e02d9a7
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-30 12:59:05 -0400

    Fix name error on update action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,6 +15,13 @@ class Api::UsersController < ApplicationController
     render :show
   end
 
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      render :show
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(
