8e1bf1cc3cfd9dc65357fdf8523e18a87621e8f4
SVKratzer <samuel.kratzer1894@gmail.com>
2020-07-30 17:50:43 -0400

    Rework newsfeed

--- frontend/components/newsfeed/newsfeed.jsx
+++ frontend/components/newsfeed/newsfeed.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavBarContainer from '../navbar/navbar_container'
 
 class Newsfeed extends React.Component {
   constructor(props) {
