819a8a7306cd0d4e1269cf333042f83b15b14af5
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 13:00:10 -0400

    Change naming

--- frontend/actions/post_api_actions.js
+++ frontend/actions/post_api_actions.js
@@ -2,6 +2,7 @@ import * as PostApiUtil from '../util/post_api_util'
 
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 export const RECEIVE_POST = 'RECEIVE_POST'
+
 export const REMOVE_POST = 'REMOVE_POST'
 export const REMOVE_POSTS = 'REMOVE_POSTS'
 
