2c43c07a5282136a928c1f5ac1a6f6b084c0646e
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-01 20:13:11 -0400

    Write posts associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :body, presence: true
+
+    belongs_to :author, 
+      foreign_key: :author_id, 
+      class_name: :User
+
+    belongs_to :wall, 
+      foreign_key: :wall_id, 
+      class_name: :User
+
+    has_many :comments, 
+      foreign_key: :post_id, 
+      class_name: :Comment, 
+      dependent: :destroy
+
+    has_many :reactions, as: :reactable
+
 end
