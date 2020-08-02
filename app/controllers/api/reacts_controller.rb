d66c162c8d9b23ec38901a3c99221b6edcbfc10c
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 09:38:20 -0400

    Build reacts table

--- app/controllers/api/reacts_controller.rb
+++ app/controllers/api/reacts_controller.rb
@@ -1,2 +1,30 @@
 class ReactsController < ApplicationController
+
+  def create
+    @react = React.new(react_params)
+    if @react.save
+      render :show
+    else
+      render json: @react.errors.full_messages, status: 422
+  end
+
+  def update
+    @react = React.new(react_params)
+    if @react.save
+      render :show 
+    else
+      render json: @react.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @react = React.find_by(id: params[:id])
+    @react.destroy
+    render :show
+  end
+
+  private
+  def react_params
+    params.require(:react).permit(:reactable_id, :reactable_type, :emoji_type)
+  end
 end
