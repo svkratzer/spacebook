eb4a70a183e2560da5de408b8686d1003ca54c2b
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-01 20:47:01 -0400

    Add photo_url column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_000318) do
+ActiveRecord::Schema.define(version: 2020_08_02_004453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_000318) do
     t.integer "wall_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
     t.index ["author_id"], name: "index_posts_on_author_id"
     t.index ["wall_id"], name: "index_posts_on_wall_id"
   end
