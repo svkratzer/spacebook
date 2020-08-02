3e778cc550e87b2b5f70cb8f2923792a02a3e6c4
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 19:49:58 -0400

    Write mSTP and mDTP and connect presentational component with container

--- frontend/components/posts/post_form.jsx
+++ frontend/components/posts/post_form.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 class PostForm extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      
+    }
   }
 
   render() {
--- frontend/components/posts/post_form_container.js
+++ frontend/components/posts/post_form_container.js
@@ -1,16 +1,18 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom'
+import { createPost } from '../../actions/post_api_actions';
 import PostForm from './post_form';
 
-const mSTP = state => {
+const mSTP = (state, ownProps) => {
   return {
-
+    currentUserId: state.session.id,
+    userId: (ownProps.match.params.userId || null)
   };
 };
 
 const mDTP = dispatch => {
   return {
-
+    createPost: formPost => dispatch(createPost(formPost))
   };
 };
 
