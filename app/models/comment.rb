279056d73c29d5c6854f666432f193d6efb8eedb
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-01 20:22:45 -0400

    Create comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,29 @@
 class Comment < ApplicationRecord
+  validates :body, :author_id, presence: true
+
+  belongs_to :author, 
+    foreign_key: :author_id, 
+    class_name: :User
+
+  belongs_to :post, 
+    foreign_key: :post_id, 
+    class_name: :Post
+  
+  has_one :wall, 
+    through: :post, 
+    source: :wall
+
+  belongs_to :parent_comment, 
+    foreign_key: :parent_comment_id, 
+    optional: true, 
+    class_name: :Comment
+  
+  has_many :children, 
+    foreign_key: :parent_comment_id, 
+    class_name: :Comment, 
+    dependent: :destroy
+
+  has_one :post_author, 
+    through: :post, 
+    source: :author
 end
