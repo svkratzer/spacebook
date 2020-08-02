46cb105e012287eb20dc3c9c6e17c1d3777ee87f
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 12:57:31 -0400

    Create post api action and thunk action creators

---
+++ frontend/actions/post_api_actions.js
@@ -0,0 +1,49 @@
+import * as PostApiUtil from '../util/post_api_util'
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const RECEIVE_POST = 'RECEIVE_POST'
+export const REMOVE_POST = 'REMOVE_POST'
+export const REMOVE_POSTS = 'REMOVE_POSTS'
+
+// STANDARD ACTION CREATORS
+
+export const receivePost = (post) => ({
+  type: RECEIVE_POST,
+  post
+})
+
+export const receivePosts = (posts) => ({
+  type: RECEIVE_POSTS,
+  posts
+})
+
+export const removePost = (post) => ({
+  type: REMOVE_POST,
+  post
+})
+
+export const removePosts = () => ({
+  type: REMOVE_POSTS
+})
+
+// THUNK ACTION CREATORS
+
+export const fetchPost = postId => dispatch =>
+  PostApiUtil.getPost(postId)
+    .then(post => dispatch(receivePost(post)));
+
+export const fetchPosts = () => dispatch => 
+  PostApiUtil.getPosts()
+    .then(payload => dispatch(receivePosts(payload)));
+
+export const createPost = formPost => dispatch => 
+  PostApiUtil.createPost(formPost)
+    .then(post => dispatch(receivePost(post)));
+    
+export const updatePost = formPost => dispatch => 
+  PostApiUtil.patchPost(formPost)
+    .then(post => dispatch(receivePost(post)));
+
+export const deletePost = postId => dispatch => 
+  PostApiUtil.deletePost(postId)
+    .then(post => dispatch(removePost(post)));
