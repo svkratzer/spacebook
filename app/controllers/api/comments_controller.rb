2f045eccc35d5be8eb4175afe9f88fdd54220a25
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 09:07:25 -0400

    Make CommentsController

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,43 @@
 class CommentsController < ApplicationController
+
+  def index
+    @post = Post.find_by(post_id: params[:post_id])
+    @comments = @post.comments.order("comments.created_at DESC")
+    render :index
+  end
+
+  def show
+    @comment = Comment.find_by(id: params[:id])
+    render :show
+  end
+
+  def create 
+    @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+    if @comment.save
+      render :show
+    else 
+      render json: @comment.errors.full_messages  
+    end
+  end
+
+  def update 
+    @comment = Comment.find_by(id: params[:id])
+    if @comment.update(comment_params)
+      render :show
+    else 
+      render json: @comment.errors.full_messages
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy
+    render :show
+  end  
+
+  private
+  def comment_params
+      params.require(:comment).permit(:body, :author_id, :post_id, :parent_comment_id)
+  end
 end
