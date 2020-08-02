c3eebb66f83d5f468d713b921a30a9175ec54963
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-01 21:04:58 -0400

    Write posts controller actions

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,41 @@
 class PostsController < ApplicationController
+  def index
+    render :index
+  end
+
+  def show 
+    @post = Post.find_by(id: params[:id])
+    render :show
+  end
+
+  def create 
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    if @post.save 
+        render :show
+    else 
+        render json: @post.errors.full_messages
+    end
+  end
+
+  def update 
+    @post = Post.find_by(id: params[:id])
+    if @post.update(post_params)
+      render :show
+    else 
+      render json: @post.errors.full_messages
+    end
+  end
+
+  def destroy      
+  @post = Post.find_by(params[:id])
+    @post.destroy
+    render :show
+  end
+
+  private
+  def post_params
+    # The wall_id is taken from the front_end route
+    params.require(:post).permit(:body, :photo_url, :wall_id)
+  end
 end
