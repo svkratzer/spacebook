1fc83e43ba7f0f7178d17f4a16665556f40df629
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 19:39:17 -0400

    Create Wall Component

--- frontend/components/profile/wall.jsx
+++ frontend/components/profile/wall.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import PostIndexContainer from '../posts/post_index_container';
+import PostFormContainer from '../posts/post_form_container';
 
 class Wall extends React.Component {
   
   render() {
     return(
       <div className="wall">
-        
+        <PostFormContainer />
+        <PostIndexContainer />
       </div>
     );
   }
