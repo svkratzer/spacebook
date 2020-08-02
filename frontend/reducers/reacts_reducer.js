5fdbf8e9f79e0d63fa58b8d4f366f835a506471a
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 14:20:23 -0400

    Create react reducer

--- frontend/reducers/reacts_reducer.js
+++ frontend/reducers/reacts_reducer.js
@@ -1,7 +1,16 @@
+import { merge } from 'lodash'
+import { RECEIVE_REACT, REMOVE_REACT } from '../actions/react_api_actions'
 
-const reactsReducer = (state, action) => {
+const reactsReducer = (state = {}, action) => {
+  Object.freeze(state);
   switch (action.type) {
-    
+    case RECEIVE_REACT:
+      newState[action.reaction.id] = action.reaction
+      return newState;
+    case REMOVE_REACT:
+      newState = merge({}, state);
+      delete newState[action.reactId]
+      return newState;
     default:
       return state;
   }
