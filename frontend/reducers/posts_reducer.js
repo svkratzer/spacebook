7d96bba7ada73054b39a0e8d102269dda1f2173d
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 14:05:33 -0400

    Create posts reducer

--- frontend/reducers/posts_reducer.js
+++ frontend/reducers/posts_reducer.js
@@ -1,6 +1,26 @@
+import { merge } from 'lodash';
+import { LOGOUT_CURRENT_USER } from '../actions/session_actions';
+import { 
+  RECEIVE_POSTS, 
+  RECEIVE_POST, 
+  REMOVE_POST, 
+  REMOVE_POSTS } from '../actions/post_api_actions';
 
-const postsReducer = (state, action) => {
+const postsReducer = (state = {}, action) => {
+  Object.freeze(state);
   switch(action.type) {
+    case RECEIVE_POSTS:
+      return Object.merge({}, action.posts);
+    case RECEIVE_POST:
+      return Object.merge({}, state, { [action.post.id]: action.post });
+    case REMOVE_POST:
+      let newState = merge({}, state);
+      delete newState[action.postId]
+      return newState;
+    case LOGOUT_CURRENT_USER:
+      return {};
+    case REMOVE_POSTS: 
+      return {};
     default:
       return state;
   }
