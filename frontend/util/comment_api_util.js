393304d877fd7428497cbe2d5351290d6bdc024e
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 12:03:48 -0400

    Make comment ajax request functions

---
+++ frontend/util/comment_api_util.js
@@ -0,0 +1,36 @@
+export const getComments = (postId) => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/posts/${postId}/comments`,
+  });
+};
+
+export const getComment = commentId => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/comments/${commentId}`
+  });
+};
+
+export const postComment = comment => {
+  return $.ajax({
+    method: "POST",
+    url: "/api/comments",
+    data: { comment }
+  });
+};
+
+export const patchComment = comment => {
+  return $.ajax({
+    method: "PATCH",
+    url: `/api/comments/${comment.id}`,
+    data: { comment }
+  });
+};
+
+export const deleteComment = commentId => {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/comments/${commentId}`
+  });
+};
