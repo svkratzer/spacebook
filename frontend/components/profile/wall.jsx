c423828fd8068a5ec8936b9994870ad9740464af
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 21:40:17 -0400

    Rework wall component organization

--- frontend/components/profile/wall.jsx
+++ frontend/components/profile/wall.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import PostIndexContainer from '../posts/post_index_container';
 
 class Wall extends React.Component {
+  constructor(props) {
+    super(props)
+  }
   
   render() {
     return(
       <div className="wall">
-        
         <PostIndexContainer />
       </div>
     );
