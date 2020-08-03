6d0a36cf5c10b8c1e1da07a63ee224dad93db3ff
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-02 20:51:24 -0400

    Remove postFormContainer from render

--- frontend/components/profile/wall.jsx
+++ frontend/components/profile/wall.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PostIndexContainer from '../posts/post_index_container';
-import PostFormContainer from '../posts/post_form_container';
 
 class Wall extends React.Component {
   
