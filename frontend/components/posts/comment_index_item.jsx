dc77e40260ded9310c59812fe3ea33648872ba82
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-03 17:50:54 -0400

    Map author name into props

---
+++ frontend/components/posts/comment_index_item.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { fetchUser } from '../../actions/user_api_actions';
+
+class CommentIndexItem extends React.Component {
+  constructor(props)  {
+    super(props)
+  }
+
+  componentDidMount() {
+    fetchUser(this.props.author.id);
+  }
+
+  render() {
+    if (!this.props.author) return null;
+
+    return (
+      <>  
+        <div className="commenter-name">{this.props.author.first_name} {this.props.author.last_name}</div>
+        <div>{this.props.comment.body}</div>
+      </>
+    );
+  }
+}
+
+export default CommentIndexItem;
