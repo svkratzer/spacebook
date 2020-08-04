45a8b6c33ce829416128603d35a14158b3aaafec
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-04 13:25:15 -0400

    Create friends reducer

---
+++ frontend/reducers/friends_reducer.js
@@ -0,0 +1,23 @@
+import { merge } from 'lodash';
+import {
+  RECEIVE_FRIEND,
+  RECEIVE_FRIENDS,
+  REMOVE_FRIEND,
+} from '../actions/post_api_actions';
+
+const friendsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  const newState = merge({}, state);
+
+  switch(action.type) {
+    case RECEIVE_FRIENDS:
+      return merge({}, action.users);
+    case RECEIVE_FRIEND:
+      return merge(newState, { [action.friend.friendship_id]: action.friend});
+    case REMOVE_FRIEND:
+      delete newState[action.friendshipId];
+      return newState;
+    default: 
+      return state;
+  }
+}
