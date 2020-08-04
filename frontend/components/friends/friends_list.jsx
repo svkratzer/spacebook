6aae6c1eb33df8608883d9345ca103f4aa609828
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-04 15:33:27 -0400

    Make basic html skeleton for first render

--- frontend/components/friends/friends_list.jsx
+++ frontend/components/friends/friends_list.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import friendsReducer from '../../reducers/friends_reducer';
 
 class FriendsList extends React.Component {
   constructor(props) {
     super(props)
+    debugger
+  }
 
+  componentDidMount() {
+    this.props.fetchFriends('friends', this.props.userId);
   }
 
   render() {
+    const { friends } = this.props
+
     return(
       <>
-
+        <div>Friends {friends.length}</div>
       </>
     );
   }
