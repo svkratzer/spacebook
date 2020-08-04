7ac92bbcc279734ae42a0058eb564ee077ef60d9
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-04 11:32:27 -0400

    Add friend association

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,4 +2,8 @@ class Friend < ApplicationRecord
   belongs_to :user,
     foreign_key: :friend_a_id,
     class_name: :User
+  
+  belongs_to :friend,
+    foreign_key: :friend_b_id,
+    class_name: :User
 end
