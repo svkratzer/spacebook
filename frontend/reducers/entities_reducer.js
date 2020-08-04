3ac75c36f8b8b0694d21b915a041dfdead08946b
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-04 13:28:13 -0400

    Connect friends reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import usersReducer from "./users_reducer";
 import postsReducer from "./posts_reducer"
 import commentsReducer from "./comments_reducer"
+import friendsReducer from './friends_reducer'
 import reactsReducer from "./reacts_reducer"
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   posts: postsReducer,
   comments: commentsReducer,
+  friends: friendsReducer,
   reacts: reactsReducer
 });
 
