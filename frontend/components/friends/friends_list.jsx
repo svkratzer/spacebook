317a2d010b75707b07f7e720f8b10c09c3d3c212
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-04 13:46:27 -0400

    Create friends list

---
+++ frontend/components/friends/friends_list.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class FriendsList extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return(
+      <>
+
+      </>
+    );
+  }
+}
+
+export default FriendsList;
