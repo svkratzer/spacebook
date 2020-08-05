e025e430d804e5eac00962beeee882d3cc677b26
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-05 08:49:47 -0400

    Rework userId mapping in mSTP

--- frontend/components/friends/friends_modal_container.js
+++ frontend/components/friends/friends_modal_container.js
@@ -2,18 +2,22 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import FriendsModal from './friends_modal';
 
-import { fetchFriends } from '../../actions/friend_api_actions';
+import { fetchFriends, destroyFriend } from '../../actions/friend_api_actions';
 
 const mSTP = (state, ownProps) => {
-
+  const arr = ownProps.location.pathname
+  const userId = parseInt(arr[arr.length - 1], 10);
   return {
-
+    currentUserId: state.session.id,
+    userId: userId,
+    friends: Object.values(state.entities.friends)
   };
 }
 
 const mDTP = (dispatch) => {
   return {
-    fetchFriends: (userId) => dispatch(fetchFriends(userId))
+    fetchFriends: (indexType, userId) => dispatch(fetchFriends(indexType, userId)),
+    destroyFriend: (friendshipId) => dispatch(destroyFriend(friendshipId))
   };
 }
 
