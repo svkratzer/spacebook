ae7d15e685acbf7b8ca050d2b1acebea2f024263
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-05 19:41:21 -0400

    Add post form

--- frontend/components/newsfeed/newsfeed.jsx
+++ frontend/components/newsfeed/newsfeed.jsx
@@ -6,12 +6,24 @@ import NewsfeedPostIndexContainer from './newsfeed_post_index_container'
 class Newsfeed extends React.Component {
   constructor(props) {
     super(props)
+
+    this.defaultPhoto = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
   }
   render() {
+    const currentUserPhoto = this.props.currentUser.profile_url || this.defaultPhoto;
+
     return (
       <section className="newsfeed-main">
         
         <NavBarContainer />
+
+        <div className="post-form-button-container">
+          <img src={currentUserPhoto} />
+          <div className="post-form-button"
+            onClick={this.props.openModalPostForm}>
+            <span>What's on your mind, {currentUser.first_name}?</span>
+          </div>
+        </div>
 
         <NewsfeedPostIndexContainer />
       </section>
