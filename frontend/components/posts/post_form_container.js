565e0967857f89b2dcbae8d73d14ce708f37ecda
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-05 19:45:34 -0400

    Remove debuggers

--- frontend/components/posts/post_form_container.js
+++ frontend/components/posts/post_form_container.js
@@ -6,8 +6,15 @@ import PostForm from './post_form';
 
 
 const mSTP = (state, ownProps) => {
-  const arr = ownProps.location.pathname
-  const userId = parseInt(arr[arr.length - 1], 10);
+  const arr = ownProps.location.pathname.split('/')
+  let userId = null;
+  if (arr[arr.length - 1] === 'newsfeed') {
+    userId = state.session.id
+  } else {
+    userId = parseInt(arr[arr.length - 1], 10);
+  }
+
+  
   return {
     currentUserId: state.session.id,
     userId: userId
