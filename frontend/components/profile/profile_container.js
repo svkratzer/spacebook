8ada7641985682d1d21dbab4e41d71c7678d2fd4
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-05 08:50:45 -0400

    Map in an array of the profile's associated friend ids

--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -1,23 +1,27 @@
 import { connect } from 'react-redux';
-import { logout } from '../../actions/session_api_actions';
 import Profile from './profile';
 import { withRouter } from 'react-router-dom';
 import { fetchUser } from '../../actions/user_api_actions';
 import { openModal } from '../../actions/modal_actions'
+import { createFriend, destroyFriend } from '../../actions/friend_api_actions'
 
 const mSTP = (state, ownProps) => {
-  
+  const friends = Object.values(state.entities.friends)
+  const friendsIds = friends.map(friend => (friend.friend_id));
   return {
     currentUserId: state.session.id,
     userId: ownProps.match.params.userId,
-    user: state.entities.users[ownProps.match.params.userId]
+    user: state.entities.users[ownProps.match.params.userId],
+    friendsIds: friendsIds
   };
 };
 
 const mDTP = (dispatch) => {
   return {
     fetchUser: userId => dispatch(fetchUser(userId)),
-    openModalPostForm: () => dispatch(openModal('postForm'))
+    openModalPostForm: () => dispatch(openModal('postForm')),
+    createFriend: (friendship, userId) => dispatch(createFriend(friendship, userId)),
+    destroyFriend: (friendshipId) => dispatch(destroyFriend(friendshipId))
   }
 }
 
