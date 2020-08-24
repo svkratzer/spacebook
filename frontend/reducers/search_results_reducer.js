9d6d4ed950e23627f619fce534f8db6a44546cd8
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-24 11:18:02 -0400

    Define _defaultResults

--- frontend/reducers/search_results_reducer.js
+++ frontend/reducers/search_results_reducer.js
@@ -1,7 +1,9 @@
 import { RECEIVE_USERS, CLEAR_USERS } from '../actions/user_api_actions'
 import { merge } from 'lodash';
 
-const searchResultsReducer = (state = _defaultUsers, action) => {
+const _defaultResults = {};
+
+const searchResultsReducer = (state = _defaultResults, action) => {
   switch (action.type) {
     case RECEIVE_USERS:
       return merge({}, state, action.users);
