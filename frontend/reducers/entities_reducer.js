728d297f286e892a59171d4df46746098e2fade3
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-24 11:16:53 -0400

    Merge search results reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,13 +4,15 @@ import postsReducer from "./posts_reducer"
 import commentsReducer from "./comments_reducer"
 import friendsReducer from './friends_reducer'
 import reactsReducer from "./reacts_reducer"
+import searchResultsReducer from './search_results_reducer'
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   posts: postsReducer,
   comments: commentsReducer,
   friends: friendsReducer,
-  reacts: reactsReducer
+  reacts: reactsReducer,
+  search: searchResultsReducer
 });
 
 export default entitiesReducer;
