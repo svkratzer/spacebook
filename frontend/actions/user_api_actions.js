641556349d5b76f9496ed44acaaa87ae7849c291
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-24 11:12:48 -0400

    Add clearUsers action creator for search functionality

--- frontend/actions/user_api_actions.js
+++ frontend/actions/user_api_actions.js
@@ -2,6 +2,7 @@ import * as UserApiUtil from '../util/user_api_util'
 
 export const RECEIVE_USER = "RECEIVE_USER";
 export const RECEIVE_USERS = "RECEIVE_USERS";
+export const CLEAR_USERS = "CLEAR_USERS";
 
 const receiveUser = user => ({
   type: RECEIVE_USER,
@@ -11,20 +12,25 @@ const receiveUser = user => ({
 const receiveUsers = users => ({
   type: RECEIVE_USERS,
   users
-})
+});
+
+const clearUsers = () => ({
+  type: CLEAR_USERS
+});
 
 export const fetchUser = userId => dispatch => (
   UserApiUtil.getUser(userId)
     .then(user => dispatch(receiveUser(user)))
 );
 
+export const updateUser = user => dispatch => (
+  UserApiUtil.updateUser(user)
+  .then(user => dispatch(receiveUser(user)))
+  );
+
+
+// FOR SEARCH
 export const fetchUsers = name => dispatch => (
   UserApiUtil.getUsers(name)
     .then(users => dispatch(receiveUsers(users)))
 );
-
-export const updateUser = user => dispatch => (
-  UserApiUtil.updateUser(user)
-    .then(user => dispatch(receiveUser(user)))
-);
-
