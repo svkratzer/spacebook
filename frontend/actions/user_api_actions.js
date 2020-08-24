ec52ebe885b1a36f6ec0be898373123a65a76e23
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-24 11:08:59 -0400

    Add fetchUsers action

--- frontend/actions/user_api_actions.js
+++ frontend/actions/user_api_actions.js
@@ -1,15 +1,26 @@
 import * as UserApiUtil from '../util/user_api_util'
 
 export const RECEIVE_USER = "RECEIVE_USER";
+export const RECEIVE_USERS = "RECEIVE_USERS";
 
 const receiveUser = user => ({
   type: RECEIVE_USER,
   user
 });
 
+const receiveUsers = users => ({
+  type: RECEIVE_USERS,
+  users
+})
+
 export const fetchUser = userId => dispatch => (
   UserApiUtil.getUser(userId)
     .then(user => dispatch(receiveUser(user)))
+);
+
+export const fetchUsers = name => dispatch => (
+  UserApiUtil.getUsers(name)
+    .then(users => dispatch(receiveUsers(users)))
 );
 
 export const updateUser = user => dispatch => (
