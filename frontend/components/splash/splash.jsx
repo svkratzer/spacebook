ffbce95c2a970b2ab5b9fcd1d9717694ff51465b
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-25 19:49:38 -0400

    Import faker

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LoginFormContainer from './login_form_container';
 import SignupFormContainer from './signup_form_container';
+import faker from 'faker';
 
 class Splash extends React.Component {
   constructor(props) {
