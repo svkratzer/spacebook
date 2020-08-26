2a49509ddcd8a0ba0c6a8f19b12a409a6b8515ee
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-26 11:24:23 -0400

    Seed friends

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 
 require "faker"
 
-NUM_USERS = 20
+NUM_USERS = 50
 NUM_POSTS_PER_USER = 10
+NUM_FRIENDS_PER_USER = 20
 
 
 User.destroy_all
@@ -36,7 +37,12 @@ profile_urls = [
   "https://vignette.wikia.nocookie.net/watchmen/images/b/b5/Rorschach_%28comic%29.jpg/revision/latest?cb=20191105041342",
   "https://i.pinimg.com/originals/46/6a/44/466a44b5bea895f14646589705ab62cd.jpg",
   "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/72LCRDBLGZFNFN2ESVARBVYLTY.jpg&w=1440",
-  "https://media.distractify.com/brand-img/uUHiKVnj-/0x0/so_104_still-05-2-1588988675988.jpg"
+  "https://media.distractify.com/brand-img/uUHiKVnj-/0x0/so_104_still-05-2-1588988675988.jpg",
+  "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c76b7d331358e35dd2773a9%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D4401%26cropY1%3D0%26cropY2%3D4401",
+  "https://img.nbc.com/sites/nbcunbc/files/metaverse_assets/1/0/2/7/1/0/ron-500x500.jpg",
+  "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg",
+  "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
+  "https://pmcdeadline2.files.wordpress.com/2016/03/ali-g.jpg?w=759"
 ]
 
 cover_urls = [
@@ -51,6 +57,9 @@ cover_urls = [
   "https://rooteto.files.wordpress.com/2012/04/1-nature-facebook-covers.jpg"
 ]
 
+new_user_ids = []
+
+# Create new users and add their IDs to the new_user_ids array
 NUM_USERS.times do 
   user = User.new(
     first_name: Faker::Name.first_name,
@@ -60,16 +69,47 @@ NUM_USERS.times do
     birthday: "1996-12-02",
     profile_url: profile_urls.sample,
     cover_url: cover_urls.sample,
-    bio: "This is a randomly seeded user."
+    bio: "This is a randomly generated user."
   )
-
   user.save!
+  new_user_ids << user.id
+end
 
+# Create a few randomly generated posts to mix things up a bit...
+NUM_RANDOM_POSTS = NUM_USERS * 5
+NUM_RANDOM_POSTS.times do 
+  Post.create(
+      author_id: new_user_ids.sample, 
+      wall_id: new_user_ids.sample, 
+      body: Faker::TvShows::RickAndMorty.quote
+    )
+end
+
+# Iterate over the new users and make them add posts to random, other users walls
+new_user_ids.each do |id|
+  # Create a post with a Hitchhikers reference
   NUM_POSTS_PER_USER.times do 
     Post.create(
-      author_id: user.id, 
-      wall_id: user.id, 
+      author_id: id, 
+      wall_id: new_user_ids.sample, 
       body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+    )
+  # Create a post with a Princess Bride reference
+    Post.create(
+      author_id: id, 
+      wall_id: new_user_ids.sample, 
+      body: Faker::Movies::PrincessBride.quote
+    )
+  end
+end
+
+# Create friends for users
+new_user_ids.each_with_index do |id, index|
+  count = 1
+  NUM_FRIENDS_PER_USER.times do 
+    Friend.create(
+      friend_a_id: id,
+      friend_b_id: new_user_ids[(index + count) % NUM_USERS]
     )
   end
 end
