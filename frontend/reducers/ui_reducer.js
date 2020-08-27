10a43c6b4d3432cd6bda94ac67319bede2aeeeb3
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-27 19:23:05 -0400

    Remove indexTypeReducer

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,11 +1,10 @@
 import { combineReducers } from 'redux';
 
 import modalReducer from './modal_reducer';
-import indexTypeReducer from './index_type_reducer';
+
 
 const uiReducer = combineReducers({
   modal: modalReducer,
-  index: indexTypeReducer
 });
 
 export default uiReducer;
