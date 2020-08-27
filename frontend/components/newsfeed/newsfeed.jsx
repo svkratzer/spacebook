be1ce2352e041395d8480ff41b33459bc32b9d84
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-27 12:00:04 -0400

    Add links to left side of the newsfeed

--- frontend/components/newsfeed/newsfeed.jsx
+++ frontend/components/newsfeed/newsfeed.jsx
@@ -20,20 +20,48 @@ class Newsfeed extends React.Component {
     const currentUserPhoto = this.props.currentUser.profile_url || this.defaultPhoto;
 
     return (
-      <section className="newsfeed-main">
-        
+      <>
         <NavBarContainer />
+        <section className="newsfeed-main">
 
-        <div className="post-form-button-container">
-          <img src={currentUserPhoto} />
-          <div className="post-form-button"
-            onClick={this.props.openModalPostForm}>
-            <span>What's on your mind, {currentUser.first_name}?</span>
+          <div className="newsfeed-links sidebar">
+            <div className="info">
+              Welcome to MyFace! Below you'll find some useful links, like the project's Github repo and the creator's Portfolio. 
+            </div>
+            <a href="https://github.com/svkratzer/MyFace">
+              <i className="fab fa-github"></i>
+              <div>Github</div>
+            </a>
+            <a href="https://svkratzer.github.io/">
+              <i className="fas fa-book-reader"></i>
+              <div>Portfolio</div>
+            </a>
+            <a href="">
+              <i className="fab fa-linkedin"></i>
+              <div>LinkedIn</div>
+            </a>
+            <a href="">
+              <i className="fab fa-angellist"></i>
+              <div>AngelList</div>
+            </a>
           </div>
-        </div>
 
-        <NewsfeedPostIndexContainer />
-      </section>
+          <div className="newsfeed-center">
+            <div className="post-form-button-container">
+              <img src={currentUserPhoto} />
+              <div className="post-form-button"
+                onClick={this.props.openModalPostForm}>
+                <span>What's on your mind, {currentUser.first_name}?</span>
+              </div>
+            </div>
+            <NewsfeedPostIndexContainer />
+          </div>
+
+          <div className="suggested-friends sidebar">
+
+          </div>
+        </section>
+      </>
     );
   }
 }
